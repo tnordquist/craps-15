3bf31f9aebaa8c5459a403e9fb02690b1224add6
Todd Nordquist <toddnordquist144@gmail.com>
2022-04-05 10:57:57 -0600

    Define Round in progress.

---
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -0,0 +1,8 @@
+package edu.cnm.deepdive;
+
+public class Main {
+
+  public static void main(String[] args) {
+  }
+
+}
---
+++ src/main/java/edu/cnm/deepdive/Round.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive;
+
+public class Round {
+
+  private State state;
+  private int point;
+
+  public Round() {
+    this.state = State.initial();
+  }
+}
